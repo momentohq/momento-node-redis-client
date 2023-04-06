c80157cc7ebcbf42655b78ff18f972d6a8f60729
Michael Landis <michael@momentohq.com>
2023-04-06 22:30:17 +0000

    feat: emit error on unimplemented method calls
    We dynamically add stub methods for all Redis commands provided
    by @redis/client that we do not implemented. The stubs emit an error through the EventEmitter
     with a human-readable message.

---
+++ test/unimplemented-commands.test.ts
@@ -0,0 +1,29 @@
+import {SetupIntegrationTest, isRedisBackedTest} from './integration-setup';
+//import {sleep} from './utils';
+
+const {client} = SetupIntegrationTest();
+
+describe('uninmplemented commands', () => {
+  it('should emit an error when the command is not implemented', async () => {
+    if (isRedisBackedTest()) {
+      return;
+    }
+    const errors: Error[] = [];
+    client.on('error', (err: Error) => {
+      errors.push(err);
+    });
+
+    await client.hGet('foo', 'bar');
+    expect(errors.length).toBe(1);
+    expect(errors[0].message).toBe(
+      'Command hGet is not implemented in MomentoRedisClient'
+    );
+    errors.pop();
+
+    await client.lPushX('foo', 'bar');
+    expect(errors.length).toBe(1);
+    expect(errors[0].message).toBe(
+      'Command lPushX is not implemented in MomentoRedisClient'
+    );
+  });
+});
