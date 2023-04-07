8d3977df3ea69b9f87a22935cd2ddf8c236c1056
Michael Landis <michael@momentohq.com>
2023-04-07 16:38:31 +0000

    fix: fix tests to expect exceptions

--- test/unimplemented-commands.test.ts
+++ test/unimplemented-commands.test.ts
@@ -8,12 +8,25 @@ describe('uninmplemented commands', () => {
       return;
     }
 
-    expect(await client.hGet('foo', 'bar')).toThrow(
-      new TypeError('Command hGet is not implemented in MomentoRedisClient')
-    );
+    const testAwaitedException = async (
+      fn: () => Promise<void>,
+      message: string
+    ) => {
+      try {
+        await fn();
+        throw new Error('should not reach here');
+      } catch (err) {
+        expect(err).toBeInstanceOf(TypeError);
+        expect((err as Error).message).toBe(message);
+      }
+    };
 
-    expect(await client.hGet('foo', 'bar')).toThrow(
-      new TypeError('Command lPushX is not implemented in MomentoRedisClient')
-    );
+    await testAwaitedException(async () => {
+      await client.hGet('foo', 'bar');
+    }, 'Command hGet is not implemented in MomentoRedisClient');
+
+    await testAwaitedException(async () => {
+      await client.lPushX('foo', 'bar');
+    }, 'Command lPushX is not implemented in MomentoRedisClient');
   });
 });
