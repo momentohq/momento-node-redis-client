96c7826cefacc5421a0c52c65333fe715c10cac4
Michael Landis <michael@momentohq.com>
2023-04-07 16:48:26 -0700

    feat: throw typeerror on unimplemented set options (#10)
    Because we do not implement all of the Redis set command options, we throw an error when a user invokes set with one that is unsupported.

--- test/unimplemented-commands.test.ts
+++ test/unimplemented-commands.test.ts
@@ -2,24 +2,24 @@ import {SetupIntegrationTest, isRedisBackedTest} from './integration-setup';
 
 const {client} = SetupIntegrationTest();
 
+const testAwaitedException = async (
+  fn: () => Promise<void>,
+  message: string
+) => {
+  try {
+    await fn();
+    throw new Error('should not reach here');
+  } catch (err) {
+    expect(err).toBeInstanceOf(TypeError);
+    expect((err as Error).message).toBe(message);
+  }
+};
+
 describe('uninmplemented commands', () => {
   it('should throw an error when the command is not implemented', async () => {
     if (isRedisBackedTest()) {
       return;
     }
-
-    const testAwaitedException = async (
-      fn: () => Promise<void>,
-      message: string
-    ) => {
-      try {
-        await fn();
-        throw new Error('should not reach here');
-      } catch (err) {
-        expect(err).toBeInstanceOf(TypeError);
-        expect((err as Error).message).toBe(message);
-      }
-    };
 
     await testAwaitedException(async () => {
       await client.hGet('foo', 'bar');
@@ -28,5 +28,35 @@ describe('uninmplemented commands', () => {
     await testAwaitedException(async () => {
       await client.lPushX('foo', 'bar');
     }, 'Command lPushX is not implemented in MomentoRedisClient');
+  });
+
+  it('should throw an error when an unsupported set option KEEPTTL is used', async () => {
+    if (isRedisBackedTest()) {
+      return;
+    }
+
+    await testAwaitedException(async () => {
+      await client.set('foo', 'bar', {KEEPTTL: true});
+    }, 'SetOption KEEPTTL is not implemented in MomentoRedisClient');
+  });
+
+  it('should throw an error when an unsupported set option XX is used', async () => {
+    if (isRedisBackedTest()) {
+      return;
+    }
+
+    await testAwaitedException(async () => {
+      await client.set('foo', 'bar', {XX: true});
+    }, 'SetOption XX is not implemented in MomentoRedisClient');
+  });
+
+  it('should throw an error when an unsupported set option GET is used', async () => {
+    if (isRedisBackedTest()) {
+      return;
+    }
+
+    await testAwaitedException(async () => {
+      await client.set('foo', 'bar', {GET: true});
+    }, 'SetOption GET is not implemented in MomentoRedisClient');
   });
 });
